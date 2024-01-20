a2e9a62caa3ee00ef99ac5b548f5b32459a756f0
Ankesh Kumar <ankesh9053@gmail.com>
2024-01-20 17:17:45 +0530

    donw withe some memory allocation question

--- Pointer.c
+++ Pointer.c
@@ -339,3 +339,28 @@ int memoryallocation2()
     return 0;
 }
 
+int rellock()
+{
+    int *ptr, i, n1, n2;
+    printf("Enter size: ");
+    scanf("%d", &n1);
+
+    ptr = (int *)malloc(n1 * sizeof(int));
+
+    printf("Addresses of previously allocated memory:\n");
+    for (i = 0; i < n1; ++i)
+        printf("%pc\n", ptr + i);
+
+    printf("\nEnter the new size: ");
+    scanf("%d", &n2);
+
+    ptr = realloc(ptr, n2 * sizeof(int));
+
+    printf("Addresses of newly allocated memory:\n");
+    for (i = 0; i < n2; ++i)
+        printf("%pc\n", ptr + i);
+
+    free(ptr);
+
+    return 0;
+}
