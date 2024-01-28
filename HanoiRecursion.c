a7627aab3ad497dbae6374fd449a4aea263aa36d
Ankesh Kumar <ankesh9053@gmail.com>
2024-01-28 20:04:48 +0530

    one more program is added

--- HanoiRecursion.c
+++ HanoiRecursion.c
@@ -78,3 +78,24 @@ int fibonacci(int prNo, int num, int term)
 
 
 
+// Write a program in C to print the array elements using recursion. >
+// Test Data :
+// Input the number of elements to be stored in the array :6
+// Input 6 elements in the array :
+// element - 0 : 2
+// element - 1 : 4
+// element - 2 : 6
+// element - 3 : 8
+// element - 4 : 10
+// element - 5 : 12
+
+int ArrayElement(int arr1[], int st, int l)
+{
+    if(st >= l)
+        return;
+         
+    printf("%d  ", arr1[st]);
+     
+   
+    ArrayElement(arr1, st+1, l);
+}
